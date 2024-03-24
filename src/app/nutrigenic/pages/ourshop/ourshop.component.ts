fa9681e6bd6b54d45e26473014322aab6329f34a
n.nasiri <n.nasirizadeh@gmail.com>
2024-03-24 08:28:16 +0430

    Add inc & dec in our shop

--- src/app/nutrigenic/pages/ourshop/ourshop.component.ts
+++ src/app/nutrigenic/pages/ourshop/ourshop.component.ts
@@ -10,6 +10,7 @@ export class OurShopComponent {
     tabItems = ['Snacks', 'Health boost ingredients', 'Fine Supplements', 'Vitamins'];
     selectedMasterTab: string | null = 'Snacks';
     listCardData: any[] = [];
+    counter:number=0;
     constructor(private router: Router) {}
 
     ngOnInit(){
@@ -18,51 +19,56 @@ export class OurShopComponent {
                 imgUrl: '../../../../assets/images/masterImage.png',
                 title: 'Almond Protein',
                 description: 'The best part is that green leafy vegetables',
-                price: '$206.60'
+                price: '€206.60'
             },
             {
                 imgUrl: '../../../../assets/images/masterImage.png',
                 title: 'Almond Protein',
                 description: 'The best part is that green leafy vegetables',
-                price: '$206.60'
+                price: '€206.60'
             },
             {
                 imgUrl: '../../../../assets/images/masterImage.png',
                 title: 'Almond Protein',
                 description: 'The best part is that green leafy vegetables',
-                price: '$206.60'
+                price: '€206.60'
             },
             {
                 imgUrl: '../../../../assets/images/masterImage.png',
                 title: 'Almond Protein',
                 description: 'The best part is that green leafy vegetables',
-                price: '$206.60'
+                price: '€206.60'
             },
             {
                 imgUrl: '../../../../assets/images/masterImage.png',
                 title: 'Almond Protein',
                 description: 'The best part is that green leafy vegetables',
-                price: '$206.60'
+                price: '€206.60'
             },
             {
                 imgUrl: '../../../../assets/images/masterImage.png',
                 title: 'Almond Protein',
                 description: 'The best part is that green leafy vegetables',
-                price: '$206.60'
+                price: '€206.60'
             },
             {
                 imgUrl: '../../../../assets/images/masterImage.png',
                 title: 'Almond Protein',
                 description: 'The best part is that green leafy vegetables',
-                price: '$206.60'
+                price: '€206.60'
             },
             {
                 imgUrl: '../../../../assets/images/masterImage.png',
                 title: 'Almond Protein',
                 description: 'The best part is that green leafy vegetables',
-                price: '$206.60'
+                price: '€206.60'
             },
          ];
     }
-
+    increaseBtnClick() {    
+        return this.counter = this.counter+1;    
+      }    
+      decreaseBtnClick() {    
+        return this.counter = this.counter==0? 0: this.counter-1;       
+      }  
 }
